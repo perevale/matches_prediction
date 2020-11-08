self loops and normalization are allowed, minor bug fix

src/GNNModel.py
@@ -16,7 +16,7 @@ class GNNModel(torch.nn.Module):
     def __init__(self, num_teams):
         super(GNNModel, self).__init__()
         self.conv1 = ClusterGCNConv(embed_dim, 3)
-        self.conv1 = GCNConv(embed_dim, 3, add_self_loops=False)
+        self.conv1 = GCNConv(embed_dim, 2)
 
         # self.conv1 = SAGEConv(embed_dim, 128)
         self.pool1 = TopKPooling(128, ratio=0.8)
@@ -25,7 +25,7 @@ class GNNModel(torch.nn.Module):
         self.conv3 = SAGEConv(128, 128)
         self.pool3 = TopKPooling(128, ratio=0.8)
         self.item_embedding = torch.nn.Embedding(num_embeddings=num_teams, embedding_dim=embed_dim)
-        self.lin1 = torch.nn.Linear(6, 6)
+        self.lin1 = torch.nn.Linear(4, 6)
         self.lin2 = torch.nn.Linear(16, 6)
         self.lin3 = torch.nn.Linear(6, target_dim)
         self.bn1 = torch.nn.BatchNorm1d(128)
@@ -40,48 +40,17 @@ class GNNModel(torch.nn.Module):
         x = self.item_embedding(x).reshape(-1,embed_dim)
         # x = x.squeeze(1)
 
-        # cache = [edge_index, edge_weight]
-        # self.conv1._cached_edge_index = cache
-
-
-
         # x = F.leaky_relu(self.conv1(x, edge_index))
-        x = F.leaky_relu(self.conv1(x, edge_index, edge_weight))
+        x = self.conv1(x, edge_index, edge_weight)
+        x = F.leaky_relu(x)
         get_neighbour_edge_index(data)
         # x = F.leaky_relu(self.conv2(x, edge_index))
 
+
+
+
         x = torch.cat([x[home],x[away]], dim=-1)
 
-
-
-
-
-        # x, edge_index, _, batch, _,_ = self.pool1(x, edge_index, None, batch)
-        # x1 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
-        #
-        # x = F.relu(self.conv2(x, edge_index))
-        #
-        # x, edge_index, _, batch, _,_ = self.pool2(x, edge_index, None, batch)
-        # x2 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
-        #
-        # x = F.relu(self.conv3(x, edge_index))
-        #
-        # x, edge_index, _, batch, _,_ = self.pool3(x, edge_index, None, batch)
-        # x3 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
-        #
-        # x = x1 + x2 + x3
-        #
-        # x = self.lin1(x)
-        # x = self.act1(x)
-        # x = self.lin2(x)
-        # x = self.act2(x)
-        # x = F.dropout(x, p=0.5, training=self.training)
-        #
-        # # mad bullshit:
-        # # ei_home = (edge_index[0] == home).nonzero().t()[0]
-        # # ei_away = (edge_index[1] == away).nonzero().t()[0]
-        # # node_indeces = np.intersection1(ei_home, ei_away)
-        #
         x = F.leaky_relu(self.lin1(x))
         # x = F.leaky_relu(self.lin2(x))
         x = F.leaky_relu(self.lin3(x))

src/utils.py
@@ -56,8 +56,8 @@ def get_neighbour_edge_index(data, self_loops=False):
     # edge_index = torch.tensor([new_from_nodes, new_from_nodes])
 
     # --------------------------------
-    new_to_nodes = np.where(data.win_lose_network[0][to_nodes, 0] == 1)
-    rep_times = np.sum(data.win_lose_network[0][to_nodes, 0], axis = 1)
+    new_to_nodes = np.where(data.win_lose_network[0][to_nodes, 0] == 1)[1]
+    rep_times = np.sum(data.win_lose_network[0][to_nodes, 0], axis = 1).astype(int)
     # if not self_loops:
     #     new_neighbours = ma.masked_array(new_neighbours, mask=)
     new_from_nodes = np.repeat(to_nodes, rep_times)
@@ -78,11 +78,14 @@ def update_edge_time(data, home, away, curr_time):
     data.edge_time[0][home][away] = curr_time
 
 
-def calculate_edge_weight(data, curr_time):
+def calculate_edge_weight(data, curr_time, time_weighting="linear"):
     from_nodes = data.edge_index[0].numpy()
     to_nodes = data.edge_index[1].numpy()
 
     prev_edge_time = data.edge_time[0][from_nodes, to_nodes]
     prev_edge_time[np.isnan(prev_edge_time)] = curr_time
 
-    data.edge_weight = (1 - ((curr_time - prev_edge_time) / data.n_datapoints[0])).reshape(-1, 1).float()
+    if time_weighting == "linear":
+        data.edge_weight = (1 - ((curr_time - prev_edge_time) / data.n_datapoints[0])).reshape(-1,).float()
+    elif time_weighting == "exponential":
+        data.edge_weight = torch.tensor(np.exp(curr_time - prev_edge_time)).reshape(-1,)