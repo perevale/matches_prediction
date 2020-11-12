b05ea65f9b7a2726e5061c72cad53bd1ed6c0512
Aleksandra Pereverzeva <perevale@fel.cvut.cz>
2020-11-12 16:49:58 +0100

    disabled InMemoryDataset

--- src/PRDataset.py
+++ src/PRDataset.py
@@ -1,28 +1,15 @@
 import numpy as np
 import torch
 from torch_geometric.data import Data
-from torch_geometric.data import InMemoryDataset
 from tqdm import tqdm
 
 from DataTransformer import DataTransformer
 
 
-class PRDataset(InMemoryDataset):
-    def __init__(self, root, filename, transform=None, pre_transform=None):
-        super(PRDataset, self).__init__(root, transform, pre_transform)
-        self.data, self.slices = torch.load(self.processed_paths[0])
+class PRDataset():
+    def __init__(self, filename):
+        # self.data, self.slices = torch.load(self.processed_paths[0])
         self.filename = filename
-
-    @property
-    def raw_file_names(self):
-        return []
-
-    @property
-    def processed_file_names(self):
-        return ['../data/GER.dataset']
-
-    def download(self):
-        pass
 
     def process(self):
         data_list = []
