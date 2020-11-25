afa363f294f484899d9bf39f85f8b04856bd912b
Aleksandra Pereverzeva <perevale@fel.cvut.cz>
2020-11-25 17:46:17 +0100

    changes for ARMA training #2

--- scripts/grid_search_run_jobs.py
+++ scripts/grid_search_run_jobs.py
@@ -12,17 +12,17 @@ DIR = "/home/perevale/thesis/parameters"
 embed_dim = [1, 3, 10]
 n_conv = [1, 2, 3]
 dims = [(1, 1, 1, 1), (4, 4, 4, 4), (8, 8, 8, 8)]
-
+activations = ['relu', 'tanh','leaky']
 lr = [(0.0001, 0.0001)]
 exp_counter = 2001
 for e in embed_dim:
     for n in n_conv:
         for d in dims:
-            for l in lr:
-                parameters = {"embed_dim": e, "n_conv": n, "conv_dims": d, "lr": l}
+            for a in activations:
+                parameters = {"embed_dim": e, "n_conv": n, "conv_dims": d, "act_f": a}
                 filename = DIR+"/parameters_{}.pickle".format(exp_counter)
                 save_to_pickle(filename, parameters)
-                command = "sbatch -o out1/slurm-%j.out train.batch {} {} ".format(exp_counter, filename)
+                command = "sbatch -o out_ARMA/slurm-%j.out train.batch {} {} ".format(exp_counter, filename)
                 process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
                 output, error = process.communicate()
 
