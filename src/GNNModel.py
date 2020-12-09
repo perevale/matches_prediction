923112419c51f3bd3ef66098c064119c7b2dcc56
Aleksandra Pereverzeva <perevale@fel.cvut.cz>
2020-12-09 21:50:22 +0100

    dist learn #8

--- src/GNNModel.py
+++ src/GNNModel.py
@@ -12,7 +12,7 @@ activations = {
 
 
 class GNNModel(torch.nn.Module):
-    def __init__(self, num_teams, embed_dim=10, n_conv=3, conv_dims=(16, 16, 16, 16), n_dense=4, dense_dims=(16, 16, 16, 4),
+    def __init__(self, num_teams, embed_dim=10, n_conv=3, conv_dims=(16, 16, 16, 16), n_dense=3, dense_dims=(4, 4, 4, 4),
                  act_f='leaky', **kwargs):
         super(GNNModel, self).__init__()
         self.embed_dim = embed_dim
